8590c0267673c0e19c296d1cfac133966ea58414
Pjotr Prins <pjotr.public01@thebird.nl>
2010-02-07 22:31:08 +0900

    Parsing attribute list

--- lib/input/swigxmlparser.rb
+++ lib/input/swigxmlparser.rb
@@ -1,4 +1,6 @@
 
+$:.unshift '/var/lib/gems/1.8/gems/libxml-ruby-1.1.3/lib/'
+
 require 'libxml'
 
 class SwigXMLParser
@@ -6,6 +8,7 @@ class SwigXMLParser
   include LibXML
 
   def initialize fn
+    print XML::LIBXML_VERSION
     print "Reading #{fn}\n"
     @xml = XML::Reader.file(fn) 
     # read top
@@ -16,15 +19,14 @@ class SwigXMLParser
     # if @xml.has_attributes?
     #   p @xml['addr']
     # end
-    while @xml.read_state!=XML::Reader::MODE_EOF and @xml.read_state!=XML::Reader::MODE_ERROR
+    # while @xml.read_state!=XML::Reader::MODE_EOF and @xml.read_state!=XML::Reader::MODE_ERROR
+    while @xml.read_state==1
       while @xml.name != 'cdecl' and @xml.name != 'class'
         @xml.read
       end
       name = @xml.name
-      @xml.read
-      @xml.read
-      p @xml.name
-      CDecl.parse(@xml.expand.to_s)
+      CDecl.parse_attributelist(@xml.expand)
+      @xml.next
     end
   end
 
@@ -37,11 +39,29 @@ module CDecl
 
   # A buffer gets put in consisting of an attribute list and 
   # attributes. These get returned in a Hash of attributes.
-  def CDecl.parse buf
-    print buf
+  def CDecl.parse_attributelist node
+    # print node.to_s
+    # print "\n++++\n"
+    h = Hash.new
+    node = node.each do | attributelist |
+      attributelist.each do | attribute |
+        name = attribute.name
+        if name == 'attribute'
+          h[attribute['name']] = attribute['value']
+        elsif name == 'parlist'
+          attribute.each do | parm |
+            p parm
+          end
+        elsif name == 'text' or name == 'typescope'
+          next
+        else
+          raise 'Strange '+name
+        end
+      end
+    end
+    p h
     print "\n----\n"
-    doc = XML::Parser.string(buf.to_s).parse
-    # attributes = doc.find("/attributelist")
+    # attributes = node.find("/attributelist")
     # p attributes
   end
 end
