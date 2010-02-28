d0cbc78bc5bbb4bd22263365fbf8f379b7c0b91e
Pjotr Prins <pjotr.public01@thebird.nl>
2010-02-28 21:41:19 +0530

    Working on xsl

--- test/unit/test_doxytransform.rb
+++ test/unit/test_doxytransform.rb
@@ -4,13 +4,15 @@ $srcpath = '../../'+File.dirname(__FILE__)
 $: << $srcpath+'/lib'
 
 DOXYXML=$srcpath+'/test/data/doxygen/xml/output/open_cdf.xml'
+HTMLXSL=$srcpath+'/etc/xsl/doxy2html.xsl'
+XALAN='/usr/bin/xalan'
 
-require 'output/transform/doxyxml'
+require 'output/transform/doxytransform'
 
 class TestDoxyTransform < Test::Unit::TestCase
 
   def test_open_cdf
-    true
+    print `#{XALAN} -xsl #{HTMLXSL} -in #{DOXYXML}`
   end
 
 end
