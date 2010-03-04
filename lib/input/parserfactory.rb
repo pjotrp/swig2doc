f133c03b4c25824ee6f1e9251ef38395d2344d52
Pjotr Prins <pjotr.public01@thebird.nl>
2010-03-04 22:01:26 +0530

    Help info

--- lib/input/parserfactory.rb
+++ lib/input/parserfactory.rb
@@ -7,19 +7,22 @@ class ParserFactory
   attr_reader :type
 
   def initialize fn
-    File.open(fn) do | f |
-      s = f.gets
-      s += f.gets
-      s += f.gets
-      case s
-        when /<doxygen/
-          @xmlparser = DoxyXMLParser.new(fn)
-          @type   = :doxy
-        when /<top/
-          @xmlparser = SwigXMLParser.new(fn)
-          @type   = :swig
-        else
-          raise "No parser for <#{fn}>"
+    if fn =~ /\.yaml$/
+    else
+      File.open(fn) do | f |
+        s = f.gets
+        s += f.gets
+        s += f.gets
+        case s
+          when /<doxygen/
+            @xmlparser = DoxyXMLParser.new(fn)
+            @type   = :doxy
+          when /<top/
+            @xmlparser = SwigXMLParser.new(fn)
+            @type   = :swig
+          else
+            raise "No parser for <#{fn}>"
+        end
       end
     end
   end
